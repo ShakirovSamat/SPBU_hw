f4948a30863f23b663710cf7e2bd302083fbca21
ShakirovSamat <samatx-r@mail.ru>
2023-09-13 22:43:57 +0300

    added matrix multiplication and parallel matrix multiplication refactored the code

--- SecondYearFirstSemestr/MatrixMultiplication/MatrixMultiplication/Program.cs
+++ SecondYearFirstSemestr/MatrixMultiplication/MatrixMultiplication/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using MatrixMultiplication;
 using MatrixMultiplication.Exceptions;
 
 namespace MatrixMultiplicationApp
@@ -10,14 +10,14 @@ namespace MatrixMultiplicationApp
         /// </summary>
         /// <param name="message"></param>
         /// <returns></returns>
-        public static int[,] getMatrix(String message)
+        public static Matrix CreateMatrix(String message)
         {
             while (true)
             {
                 Console.Write(message);
                 try
                 {
-                    return readMatrixFromFile(Console.ReadLine());
+                    return new Matrix(Console.ReadLine());
                 }
                 catch (Exception e)
                 {
@@ -43,71 +43,35 @@ namespace MatrixMultiplicationApp
             }
         }
 
-        /// <summary>
-        ///     reads matrix consisting of integers from file
-        /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        /// <exception cref="FileNotFoundException"></exception>
-        /// <exception cref="FileIsEmptyException"></exception>
-        /// <exception cref="NotAMatrixException"></exception>
-        /// <exception cref="BadMatrixElementException"></exception>
-        public static int[,] readMatrixFromFile(String path)
-        {
-            if (!File.Exists(path))
-            {
-                throw new FileNotFoundException();
-            }
-
-            StreamReader streamReader = new StreamReader(path);
-            String fileData = streamReader.ReadToEnd();
-            if (fileData.Length == 0)
-            {
-                streamReader.Close();
-                throw new FileIsEmptyException();
-            }
-
-            String[] matrixLines = fileData.Split('\n', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-            int matrixHight = matrixLines.Length;
-            int matrixWidth = matrixLines[0].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Length;
-            int[,] matrix = new int[matrixHight, matrixWidth];
-            for (int i = 0; i < matrixHight; ++i)
-            {
-                String[] numbers = matrixLines[i].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-                if (numbers.Length != matrixWidth)
-                {
-                    streamReader.Close();
-                    throw new NotAMatrixException();
-                }
-
-                for (int j = 0; j < matrixWidth; ++j)
-                {
-                    if (!Int32.TryParse(numbers[j], out matrix[i, j]))
-                    {
-                        streamReader.Close();
-                        throw new BadMatrixElementException();
-                    }
-                }
-            }
-
-            streamReader.Close();
-            return matrix;
-        }
-
-        public static int[,] multiplicateMatrix(int[,] firstMatrix, int[,] secondMatrix)
-        {
-            if (firstMatrix.GetLength(1) != secondMatrix.GetLength(2)) throw new MatrixMultiplicationException();
-
-            return null;
-        }
-
         public static void Main(String[] args)
         {
             Console.WriteLine("*****Matrix Multiplication Application*****");
             Console.WriteLine("This program multiplicates two matrix");
 
-            int[,] firstMatrix = getMatrix("Enter path to the first matrix: ");
-            int[,] secondMatrix = getMatrix("Enter path to the second matrix: ");
+            var firstMatrix = CreateMatrix("Enter path to the first matrix: ");
+            var secondMatrix = CreateMatrix("Enter path to the second matrix: ");
+
+           
+            var multiplicatedMatrix = Matrix.parallelMultiplicateMatrix(firstMatrix, secondMatrix);
+
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            Console.WriteLine("The result will be saved to Desktop");
+            while (true)
+            {
+                Console.Write("Enter name of the saving file: ");
+                var name = Console.ReadLine().Trim();
+                try
+                {
+                    Matrix.writeMatrixToFile(multiplicatedMatrix, path + "\\" + name + ".txt");
+                    break;
+                }
+                catch (FileAlreadyExistException)
+                {
+                    Console.WriteLine("Such file already exists");
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
--- SecondYearFirstSemestr/MatrixMultiplication/MatrixMultiplicationTests/UnitTest1.cs
+++ SecondYearFirstSemestr/MatrixMultiplication/MatrixMultiplicationTests/UnitTest1.cs
@@ -1,16 +1,56 @@
+using MatrixMultiplication;
+using System.Diagnostics;
+
 namespace MatrixMultiplicationTests
 {
     public class Tests
     {
+        Matrix matrix;
+        int n;
         [SetUp]
         public void Setup()
         {
+            matrix = new Matrix(500, 500);
+            Matrix.fillMatrix(matrix, 1);
+            n = 10;
         }
 
         [Test]
-        public void Test1()
+        public void AvarageTimeOfParallelMultiplication()
         {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < n; ++i)
+            {
+                Matrix.parallelMultiplicateMatrix(matrix, matrix);
+            }
+
+            sw.Stop();
+            TestContext.Out.WriteLine("Parallel multiplication time: " + sw.Elapsed.TotalSeconds / n);
             Assert.Pass();
         }
+
+        [Test]
+        public void AvarageTimeOfSimplelMultiplication()
+        {
+            Matrix.fillMatrix(matrix, 1);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < n; ++i)
+            {
+                Matrix.multiplicateMatrix(matrix, matrix);
+            }
+
+            sw.Stop();
+            TestContext.Out.WriteLine("Simple multiplication time: " + sw.Elapsed.TotalSeconds / n);
+            Assert.Pass();
+        }
+
+        [Test]
+        public void ExpectedValueTest()
+        {
+
+        }
+    
     }
 }
