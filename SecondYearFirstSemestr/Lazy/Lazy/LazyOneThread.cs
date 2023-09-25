585ca50adb92e1e9865511b0e90f2e1d04550d5e
ShakirovSamat <samatx-r@mail.ru>
2023-09-25 23:33:22 +0300

    One thread lazy calculations were added Ilazy interface were added

---
+++ SecondYearFirstSemestr/Lazy/Lazy/LazyOneThread.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lazy
+{
+    public class LazyOneThread<T>: ILazy<T>
+    {
+        private Func<T> supplier;
+        private bool isCalculated;
+        private T? value;
+        public LazyOneThread(Func<T> func)
+        {
+            supplier = func;
+            isCalculated = false;
+        }
+        public T Get()
+        {
+            if (!isCalculated)
+            {
+                value = supplier();
+                isCalculated = true;
+                return value;
+            }
+            return value;
+        }
+    }
+}
