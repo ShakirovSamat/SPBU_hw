8aee5959e819ca5ce99735535484954da308c72b
ShakirovSamat <samatx-r@mail.ru>
2023-11-02 17:03:25 +0300

    added solution

---
+++ SecondYearFirstSemestr/Test_1/Test_1/Client.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Test_1
+{
+    public class Client
+    {
+        private TcpClient client;
+        private Stream stream;
+
+        public Client()
+        {
+            client = new TcpClient();
+        }
+
+        public async Task<bool> Connect(string address, int port)
+        {
+            await client.ConnectAsync(address, port);
+            stream = client.GetStream();
+            return client.Connected;
+        }
+
+        public async Task SendMessage(string message)
+        {
+            var data = Encoding.UTF8.GetBytes(message + '\n');
+            await stream.WriteAsync(data);
+        }
+
+        public async void Chat()
+        {
+            ListenChat();
+            while (true)
+            {
+                var message = Console.ReadLine();
+                await SendMessage(message);
+            }
+        }
+
+        public void ListenChat()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    var message = new List<byte>();
+                    int bytesRead = 10;
+                    while ((bytesRead = stream.ReadByte()) != '\n')
+                    {
+                        message.Add((byte)bytesRead);
+                    }
+
+                    Console.WriteLine(Encoding.UTF8.GetString(message.ToArray()));
+                    message.Clear();
+                }
+            });
+        }
+    }
+}
